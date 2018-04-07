ce26cacc1327c999898324302ceed37c9177f3ca
Vedran <vedran@Vedrans-iMac.local>
2018-04-07 10:06:14 +0200

    tests refactored

--- src/test/java/org/cactoos/http/HtAutoRedirectTest.java
+++ src/test/java/org/cactoos/http/HtAutoRedirectTest.java
@@ -23,26 +23,18 @@
  */
 package org.cactoos.http;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.URI;
+import org.cactoos.io.InputOf;
 import org.cactoos.text.JoinedText;
 import org.cactoos.text.TextOf;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Test;
-import org.takes.Request;
-import org.takes.Response;
-import org.takes.Take;
-import org.takes.http.BkBasic;
 import org.takes.http.FtRemote;
-import org.takes.rs.RsText;
-import org.takes.rs.RsWithHeader;
-import org.takes.rs.RsWithStatus;
 import org.takes.tk.TkText;
 
 /**
  * Test case for {@link HtAutoRedirect}.
+ *
  * @author Vedran Vatavuk (123vgv@gmail.com)
  * @version $Id$
  * @since 0.1
@@ -53,13 +45,16 @@ public final class HtAutoRedirectTest {
 
     @Test
     public void redirectsRequestAutomatically() throws Exception {
-        HtAutoRedirectTest.remote().exec(
+        new FtRemote(new TkText("redirected ok")).exec(
             home -> MatcherAssert.assertThat(
                 new TextOf(
                     new HtAutoRedirect(
-                        new HtResponse(
-                            new HtWire(home),
-                            HtAutoRedirectTest.requestInput(home)
+                        new InputOf(
+                            new JoinedText(
+                                "\r\n",
+                                "HTTP/1.1 301",
+                                String.format("Location: %s", home)
+                            )
                         )
                     )
                 ).asString(),
@@ -70,129 +65,31 @@ public final class HtAutoRedirectTest {
 
     @Test
     public void noRedirectionOnStatusOk() throws Exception {
-        new FtRemote(new TkText("Hello, world!")).exec(
-            home -> MatcherAssert.assertThat(
-                new TextOf(
-                    new HtAutoRedirect(
-                        new HtResponse(
-                            new HtWire(home),
-                            HtAutoRedirectTest.requestInput(home)
-                        )
+        final String response = "HTTP/1.1 200 OK";
+        MatcherAssert.assertThat(
+            new TextOf(
+                new HtAutoRedirect(
+                    new InputOf(
+                        new TextOf(response)
                     )
-                ).asString(),
-                Matchers.containsString("HTTP/1.1 200 OK")
-            )
+                )
+            ).asString(),
+            Matchers.containsString(response)
         );
     }
 
     @Test
-    public void returnsErrorResponseForNoLocation() throws Exception {
-        new FtRemote(
-            new Take() {
-                @Override
-                public Response act(final Request req) {
-                    // @checkstyle MagicNumber (1 line)
-                    return new RsWithStatus(new RsText(), 300);
-                }
-            }).exec(
-                home -> MatcherAssert.assertThat(
-                    new TextOf(
-                        new HtAutoRedirect(
-                            new HtResponse(
-                                new HtWire(home),
-                                HtAutoRedirectTest.requestInput(home)
-                            )
-                        )
-                    ).asString(),
-                    Matchers.containsString("HTTP/1.1 300 ")
-                )
-        );
-    }
-
-    /**
-     * Request input.
-     * @param home Uri
-     * @return String Request as string
-     * @throws IOException If fails
-     */
-    private static String requestInput(final URI home) throws IOException {
-        return new JoinedText(
-            "\r\n",
-            "GET / HTTP/1.1",
-            String.format("Host:%s", home.getHost())
-        ).asString();
-    }
-
-    /**
-     * Creates front instance that returns redirect response for
-     * the first request attempt.
-     * @return FtRemote Front
-     * @throws IOException If fails
-     */
-    private static FtRemote remote() throws IOException {
-        final ServerSocket skt = HtAutoRedirectTest.randomSocket();
-        return new FtRemote(
-            new BkBasic(
-                new HtAutoRedirectTest.TkRedirect(
-                    String.format(
-                        "Location: http://localhost:%s/test",
-                        skt.getLocalPort()
+    public void returnsRedirectResponseForNoLocation() throws Exception {
+        final String response = "HTTP/1.1 300";
+        MatcherAssert.assertThat(
+            new TextOf(
+                new HtAutoRedirect(
+                    new InputOf(
+                        new TextOf(response)
                     )
                 )
-            ),
-            skt
+            ).asString(),
+            Matchers.containsString(response)
         );
-    }
-
-    /**
-     * Generates socket on a random port.
-     * @return ServerSocket Socket
-     * @throws IOException If fails
-     */
-    private static ServerSocket randomSocket() throws IOException {
-        final ServerSocket skt = new ServerSocket(0);
-        skt.setReuseAddress(true);
-        return skt;
-    }
-
-    /**
-     * Take that redirects response on a first request attempt.
-     */
-    private static final class TkRedirect implements Take {
-
-        /**
-         * Location url.
-         */
-        private final String header;
-
-        /**
-         * Number of attempts.
-         */
-        private Integer attempts;
-
-        /**
-         * Ctor.
-         * @param destination Destination url
-         */
-        TkRedirect(final String destination) {
-            this.header = destination;
-            this.attempts = 0;
-        }
-
-        @Override
-        public Response act(final Request req) {
-            final Response response;
-            if (this.attempts == 0) {
-                response = new RsWithHeader(
-                    // @checkstyle MagicNumber (1 line)
-                    new RsWithStatus(new RsText(), 301),
-                    this.header
-                );
-                this.attempts += 1;
-            } else {
-                response = new RsText("Hello World!");
-            }
-            return response;
-        }
     }
 }
