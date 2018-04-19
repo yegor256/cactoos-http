5664d8f6f72d1257acf770d46376facd191d4821
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2018-04-19 22:37:53 +0200

    #16 added default ports

--- src/main/java/org/cactoos/http/HtWire.java
+++ src/main/java/org/cactoos/http/HtWire.java
@@ -27,15 +27,16 @@ package org.cactoos.http;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URI;
+import java.util.function.BiFunction;
 import org.cactoos.Input;
 import org.cactoos.io.BytesOf;
 import org.cactoos.io.InputOf;
 
 /**
  * Wire.
- *
  * @author Yegor Bugayenko (yegor256@gmail.com)
  * @version $Id$
  * @since 0.1
@@ -58,11 +59,16 @@ public final class HtWire implements Wire {
     private final int port;
 
     /**
+     * Supplier of sockets.
+     */
+    private final BiFunction<String, Integer, Socket> supplier;
+
+    /**
      * Ctor.
      * @param uri The address of the server
      */
     public HtWire(final URI uri) {
-        this(uri.getHost(), uri.getPort());
+        this(uri, HtWire::socket);
     }
 
     /**
@@ -71,7 +77,7 @@ public final class HtWire implements Wire {
      */
     public HtWire(final String addr) {
         // @checkstyle MagicNumber (1 line)
-        this(addr, 80);
+        this(addr, 80, HtWire::socket);
     }
 
     /**
@@ -80,16 +86,40 @@ public final class HtWire implements Wire {
      * @param tcp The TCP port
      */
     public HtWire(final String addr, final int tcp) {
+        this(addr, tcp, HtWire::socket);
+    }
+
+    /**
+     * Ctor.
+     * @param uri The address of the server
+     * @param spplier Socket supplier
+     */
+    HtWire(final URI uri,
+        final BiFunction<String, Integer, Socket> spplier) {
+        this(uri.getHost(), HtWire.prt(uri), spplier);
+    }
+
+    /**
+     * Ctor.
+     * @param addr The address of the server
+     * @param tcp The TCP port
+     * @param spplier Supplier of sockets
+     */
+    HtWire(final String addr, final int tcp,
+        final BiFunction<String, Integer, Socket> spplier) {
         this.address = addr;
         this.port = tcp;
+        this.supplier = spplier;
     }
 
     @Override
     public Input send(final Input input) throws IOException {
-        try (final Socket socket = new Socket(this.address, this.port);
+        try (
+            final Socket socket = this.supplier.apply(this.address, this.port);
             final InputStream source = input.stream();
             final InputStream ins = socket.getInputStream();
-            final OutputStream ous = socket.getOutputStream()) {
+            final OutputStream ous = socket.getOutputStream()
+        ) {
             final byte[] buf = new byte[HtWire.LENGTH];
             while (true) {
                 final int len = source.read(buf);
@@ -102,4 +132,36 @@ public final class HtWire implements Wire {
         }
     }
 
+    /**
+     * Get port number from URI.
+     * @param uri URI to get the port from
+     * @return Port number
+     */
+    private static int prt(final URI uri) {
+        final int port;
+        if (uri.getPort() == -1) {
+            try {
+                port = uri.toURL().getDefaultPort();
+            } catch (final MalformedURLException ex) {
+                throw new IllegalStateException(ex);
+            }
+        } else {
+            port = uri.getPort();
+        }
+        return port;
+    }
+
+    /**
+     * Create socket from address and port.
+     * @param addr Address for the socked
+     * @param tcp Port for the socket
+     * @return Created socket
+     */
+    private static Socket socket(final String addr, final Integer tcp) {
+        try {
+            return new Socket(addr, tcp);
+        } catch (final IOException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
 }
---
+++ src/test/java/org/cactoos/http/HtWireTest.java
@@ -0,0 +1,93 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.http;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URI;
+import java.util.function.BiFunction;
+import org.cactoos.io.DeadInput;
+import org.cactoos.io.DeadInputStream;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Test case for {@link HtWire}.
+ * @author Krzysztof Krason (Krzysztof.Krason@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ * @checkstyle JavadocMethodCheck (500 lines)
+ */
+public final class HtWireTest {
+
+    /**
+     * Default port for HTTP.
+     */
+    private static final int HTTP_PORT = 80;
+
+    /**
+     * Default port for HTTPS.
+     */
+    private static final int HTTPS_PORT = 443;
+
+    @Test
+    public void guessesCorrectPortForHttp() throws IOException {
+        this.checkPorts("http://localhost", HtWireTest.HTTP_PORT);
+    }
+
+    @Test
+    public void guessesCorrectPortForHttps() throws IOException {
+        this.checkPorts("https://localhost", HtWireTest.HTTPS_PORT);
+    }
+
+    /**
+     * Verify correct port is returned for given URL.
+     * @param url URL to check
+     * @param port Port number
+     * @throws IOException In case of error
+     */
+    @SuppressWarnings("unchecked")
+    private void checkPorts(final String url, final int port)
+        throws IOException {
+        final BiFunction<String, Integer, Socket> function =
+            Mockito.mock(BiFunction.class);
+        final Socket socket = this.socket();
+        Mockito.when(function.apply(Mockito.any(), Mockito.any()))
+            .thenReturn(socket);
+        new HtWire(URI.create(url), function)
+            .send(new DeadInput());
+        Mockito.verify(function).apply(Mockito.any(), Mockito.eq(port));
+    }
+
+    /**
+     * Create a mock socket.
+     * @return A mock socket
+     * @throws IOException In case of error
+     */
+    private Socket socket() throws IOException {
+        final Socket socket = Mockito.mock(Socket.class);
+        Mockito.when(socket.getInputStream()).thenReturn(new DeadInputStream());
+        return socket;
+    }
+}
